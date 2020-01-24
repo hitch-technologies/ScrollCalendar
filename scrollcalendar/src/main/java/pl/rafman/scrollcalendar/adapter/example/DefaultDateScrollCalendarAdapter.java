1893a6b1b304ee168b11e6ad173ce597f5d4346f
Gilad Oved <giladoved@gmail.com>
2020-01-24 11:42:40 -0600

    try

--- scrollcalendar/src/main/java/pl/rafman/scrollcalendar/adapter/example/DefaultDateScrollCalendarAdapter.java
+++ scrollcalendar/src/main/java/pl/rafman/scrollcalendar/adapter/example/DefaultDateScrollCalendarAdapter.java
@@ -44,20 +44,6 @@ public class DefaultDateScrollCalendarAdapter extends ScrollCalendarAdapter {
         super.onCalendarDayClicked(year, month, day);
     }
 
-    @Override
-    protected int getStateForDate(int year, int month, int day) {
-        if (isSelected(selected, year, month, day)) {
-            return CalendarDay.SELECTED;
-        }
-        if (isToday(year, month, day)) {
-            return CalendarDay.TODAY;
-        }
-        if (isInThePast(year, month, day)) {
-            return CalendarDay.DISABLED;
-        }
-        return CalendarDay.DEFAULT;
-    }
-
     private boolean isInThePast(int year, int month, int day) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 1);
@@ -75,59 +61,73 @@ public class DefaultDateScrollCalendarAdapter extends ScrollCalendarAdapter {
         return now > then;
     }
 
-    @Override
-    protected boolean isAllowedToAddPreviousMonth() {
-        return false;
-    }
+//    @Override
+//    protected int getStateForDate(int year, int month, int day) {
+//        if (isSelected(selected, year, month, day)) {
+//            return CalendarDay.SELECTED;
+//        }
+//        if (isToday(year, month, day)) {
+//            return CalendarDay.TODAY;
+//        }
+//        if (isInThePast(year, month, day)) {
+//            return CalendarDay.DISABLED;
+//        }
+//        return CalendarDay.DEFAULT;
+//    }
 
-    @Override
-    protected boolean isAllowedToAddNextMonth() {
-        return true;
-    }
+//    @Override
+//    protected boolean isAllowedToAddPreviousMonth() {
+//        return false;
+//    }
+//
+//    @Override
+//    protected boolean isAllowedToAddNextMonth() {
+//        return true;
+//    }
 
-    private boolean isSelected(Calendar selected, int year, int month, int day) {
-        if (selected == null) {
-            return false;
-        }
-        //noinspection UnnecessaryLocalVariable
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, selected.get(Calendar.YEAR));
-        calendar.set(Calendar.MONTH, selected.get(Calendar.MONTH));
-        calendar.set(Calendar.DAY_OF_MONTH, selected.get(Calendar.DAY_OF_MONTH));
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        long millis = calendar.getTimeInMillis();
-
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        long millis2 = calendar.getTimeInMillis();
-
-        return millis == millis2;
-    }
-
-    private boolean isToday(int year, int month, int day) {
-        //noinspection UnnecessaryLocalVariable
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-
-        // Today in milliseconds
-        long today = calendar.getTime().getTime();
-
-        // Given day in milliseconds
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-
-        long calendarMillis = calendar.getTime().getTime();
-
-        return today == calendarMillis;
-    }
+//    private boolean isSelected(Calendar selected, int year, int month, int day) {
+//        if (selected == null) {
+//            return false;
+//        }
+//        //noinspection UnnecessaryLocalVariable
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.YEAR, selected.get(Calendar.YEAR));
+//        calendar.set(Calendar.MONTH, selected.get(Calendar.MONTH));
+//        calendar.set(Calendar.DAY_OF_MONTH, selected.get(Calendar.DAY_OF_MONTH));
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+//        calendar.set(Calendar.MILLISECOND, 0);
+//        long millis = calendar.getTimeInMillis();
+//
+//        calendar.set(Calendar.YEAR, year);
+//        calendar.set(Calendar.MONTH, month);
+//        calendar.set(Calendar.DAY_OF_MONTH, day);
+//        long millis2 = calendar.getTimeInMillis();
+//
+//        return millis == millis2;
+//    }
+//
+//    private boolean isToday(int year, int month, int day) {
+//        //noinspection UnnecessaryLocalVariable
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
+//        calendar.set(Calendar.MILLISECOND, 0);
+//
+//        // Today in milliseconds
+//        long today = calendar.getTime().getTime();
+//
+//        // Given day in milliseconds
+//        calendar.set(Calendar.YEAR, year);
+//        calendar.set(Calendar.MONTH, month);
+//        calendar.set(Calendar.DAY_OF_MONTH, day);
+//
+//        long calendarMillis = calendar.getTime().getTime();
+//
+//        return today == calendarMillis;
+//    }
 
     @Nullable
     public Date getSelectedDate() {
